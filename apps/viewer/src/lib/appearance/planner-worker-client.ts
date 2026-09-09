64b048dc39f5c5f5b266bc6e14e8970ade9f69a8
Louis Trümpler <78563314+louistrue@users.noreply.github.com>
2026-09-10 00:09:08 +0200

    chore(viewer): keep scan worker import below license header

--- apps/viewer/src/lib/appearance/planner-worker-client.ts
+++ apps/viewer/src/lib/appearance/planner-worker-client.ts
@@ -1,7 +1,7 @@
-import type { ScanRegistrationRequest, ScanRegistrationReport } from './scan/types';
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import type { ScanRegistrationRequest, ScanRegistrationReport } from './scan/types';
 import type { CapturedMeshPlan, CapturedMeshRequest, AnnotationPlanePlan, AnnotationPlaneRequest, PageAppearancePlan, PageAppearanceRequest, AppearanceCatalog, AppearanceCatalogRequest, AppearancePlan, AppearanceRequest, AppearanceWorkerJob, AppearanceWorkerRequest, AppearanceWorkerResponse } from './planner-types.js';
 
 export interface AppearanceWorker {
